c839a32fe6a21c8bd9b888a59fe70a9adcc70736
mutjin <mutjin23@naver.com>
2023-02-17 17:04:28 +0900

    for 230217

--- Data Structre (자료구조)/test.py
+++ Data Structre (자료구조)/test.py
@@ -1,4 +1,19 @@
-from collections import deque
-lst=deque([i for i in range(1,6)])
-lst.rotate(1)
-print(lst)
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+circles = [list(map(int, input().split())) for _ in range(N)]
+ans = 'YES'
+
+for two in combinations(circles,2):
+  rr=
+  dd=
+  if abs(two[0][1] + two[1][1]) < abs(two[0][0] - two[1][0]): # 외부
+    continue
+  elif abs(two[0][1] - two[1][1]) > abs(two[0][0] - two[1][0]): # 내부
+    continue
+  else:
+    ans = 'NO'
+    break
+print(ans)
